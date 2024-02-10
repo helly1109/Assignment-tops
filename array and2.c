4192eb9ec5f3e4e0f32982a2569b10db03598e67
helly1109 <154655029+helly1109@users.noreply.github.com>
2024-02-10 10:55:26 +0530

    Add files via upload

---
+++ array and2.c
@@ -0,0 +1,51 @@
+//2. WAP of Addition, Subtraction, Multiplication and Division using Switch
+//case.(Must Be Menu Driven)
+
+#include < stdio.h >  
+  
+int main()  
+{  
+    int a, b, choice;  
+  
+    printf("Enter your choice\n");  
+    printf("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n\n");  
+    scanf("%d", &choice);  
+  
+    if(choice > 4)  
+    {  
+        printf("You entered wrong choice\n");  
+    }  
+    else  
+    {  
+        printf("Enter 2 integer numbers\n");  
+        scanf("%d %d", &a, &b);  
+    }  
+  
+  
+    if(choice == 1)  
+    {  
+        printf("Addition of %d and %d is %d\n", a, b, (a+b));  
+    }  
+    else if(choice == 2)  
+    {  
+        printf("Subtraction of %d and %d is %d\n", a, b, (a-b));  
+    }  
+    else if(choice == 3)  
+    {  
+        printf("Multiplication of %d and %d is %d\n", a, b, (a*b));  
+    }  
+    else if(choice == 4)  
+    {  
+        if(b != 0)  
+            printf("Division of %d and %d is %d\n", a, b, (a/b));  
+        else  
+            printf("Number can not be divided by 0\n");  
+    }  
+    else  
+    {  
+        printf("Please enter any of these no 1, 2, 3, 4\n");  
+    }  
+  
+  
+    return 0;  
+
